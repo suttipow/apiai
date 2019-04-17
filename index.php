da0690defe8611450be41b2ff0788b665b8fe4de
suttipow <49715933+suttipow@users.noreply.github.com>
2019-04-17 22:48:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,20 @@
-<?php
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-
-if($method == "POST"){
-
-	$requestBody = file_get_contents('php://input');
-	$json = json_decode($requestBody);
-
-	$text = $json->result->parameters->text;
-
-    switch ($text) {
-
-    	 case 'hi':
-    	 	$speech = "Hi, Nice to meet you";
-    	 	break;
-    	 case 'bye' 
-    	 	$speech = "bye, good night";
-    	 	break;
-     	 case 'anything' 
-    	 	$speech = "yes, You can type anything here.";
-    	 	break;
-    	 default:
-    	 	$speech = "Sorry, I didnt get that. Please ask me something else."
-    	 	break;
+function processMessage($update) {
+    if($update["result"]["action"] == "sayHello"){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => "Hello from webhook",
+            "displayText" => "Hello from webhook",
+            "contextOut" => array()
+        ));
     }
-
-    $response = new \stdClass();
-    $response->speech = "";
-    $response->displayText = "";
-    $response->source = "webhook";
-    echo json_encode($response);
-}
-else
-{
-	echo "Method not allowed";
 }
 
-?>
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
+
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
+}
