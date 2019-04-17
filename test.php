bb2300e3f12aa0254037381e7264dcc31635845e
suttipow <49715933+suttipow@users.noreply.github.com>
2019-04-17 20:32:43 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,27 @@
 <?php
+  $method = $_SERVER['REQUEST_METHOD'];
+  if($method == "POST"){
 
-$jsondata = file_get_contents("test.json");
-$json = json_decode($jsondata,true);
-echo $json['sitecode'][0]['id'];
+    $jsondata = file_get_contents("test.json");
+    $json = json_decode($jsondata,true);
+  
+    $text = $json->result->parameters->text;
+    foreach($json['sitecode'] as $sitecode){
+      if($text == $sitecode['id']{
+        echo $text;
+      }
+    
+    }
+
+    $response = new \stdClass();
+    $response->speech ="";
+    $response->displayText = "";
+    $response->source = "webhook";
+ //   echo $json['sitecode'][0]['id'];
+    echo json_decode($response);
+}  
+else
+{
+    echo "Method npt allowed";
+}
 ?>
