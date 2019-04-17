67a5bc5e105c1abf4f204941632f8ffa54dc069b
suttipow <49715933+suttipow@users.noreply.github.com>
2019-04-18 00:10:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,40 @@
 <?php
-date_default_timezone_set(“Asia/Bangkok”);
-$date = date(“Y-m-d”);
-$time = date(“H:i:s”);
-$json = file_get_contents(‘php://input’);
-$request = json_decode($json, true);
-$queryText = $request[“queryResult”][“queryText”];
-$userId = $request[‘originalDetectIntentRequest’][‘payload’][‘data’][‘source’][‘userId’];
-$myfile = fopen(“log$date.txt”, “a”) or die(“Unable to open file!”);
-$log = $date.”-”.$time.”\t”.$userId.”\t”.$queryText.”\n”;
-fwrite($myfile,$log);
-fclose($myfile);
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+
+if($method == "POST"){
+
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+    switch ($text) {
+
+    	 case 'hi':
+    	 	$speech = "Hi, Nice to meet you";
+    	 	break;
+    	 case 'bye' 
+    	 	$speech = "bye, good night";
+    	 	break;
+     	 case 'anything' 
+    	 	$speech = "yes, You can type anything here.";
+    	 	break;
+    	 default:
+    	 	$speech = "Sorry, I didnt get that. Please ask me something else."
+    	 	break;
+    }
+
+    $response = new \stdClass();
+    $response->speech = "";
+    $response->displayText = "";
+    $response->source = "webhook";
+    echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
 
 ?>
