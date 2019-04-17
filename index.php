84567e31ab77fdda427a72cd8e2218477fdbe0b2
suttipow <49715933+suttipow@users.noreply.github.com>
2019-04-17 22:33:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if($method == "POST"){
 
-	$requestBody = file_get_contents('suttipow/apiai');
+	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
 	$text = $json->result->parameters->text;
