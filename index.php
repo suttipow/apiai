96c680d96852c0ac52eaa4d707a0599e4f3cb1c2
suttipow <49715933+suttipow@users.noreply.github.com>
2019-04-17 21:12:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,36 @@
 <?php
-  $method = $_SERVER['REQUEST_METHOD'];
-  if($method == "POST"){
-    $jsondata = file_get_contents("test.json");
-    $json = json_decode($jsondata,true);
-  
-    $text = $json->result->parameters->text;
-    foreach($json['sitecode'] as $sitecode){
-      if($text == $sitecode['id']{
-        echo $text;
-      }
-    
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+
+if($method == "POST"){
+
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+    switch ($text) {
+    	 case 'hi' :
+    	 	$speech = "Hi, Nice to meet you";
+    	 	break;
+    	 case 'bye' :
+    	 	$speech = "bye, good night";
+    	 	break;
+     	 case 'anything' :
+    	 	$speech = "yes, You can type anything here.";
+    	 	break;
+    	 default:
+    	 	$speech = "Sorry, I didnt get that. Please ask me something else."
+    	 	break;
     }
+
     $response = new \stdClass();
-    $response->speech ="";
+    $response->speed = "";
     $response->displayText = "";
     $response->source = "webhook";
-    echo $json['sitecode'][0]['id'];
-  //  echo json_decode($response);
-}  
-else
-{
-    echo "Method npt allowed";
+    echo json_encode($response);
+}
+else{
+	echo "Method not allowed";
 }
 ?>
