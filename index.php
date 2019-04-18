8982b4ee1592ef6d1e5cd72de8660e86f00c4817
suttipow <49715933+suttipow@users.noreply.github.com>
2019-04-18 10:28:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,44 +6,28 @@ $php_data = json_decode($json_data,true);
 $method = $_SERVER['REQUEST_METHOD'];
 
 if($method == 'POST'){
-
-
-    
+  
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
   
     //echo $json['sitecode'][1]['id'];
-    
-    
-    
      $text = $json->result->parameters->text;
-
-    if($text == $php_data['sitecode'][0]['id'])
-    {
-    $speech = "e001 This question is too personal";
     
-    }
-    else
+    foeeach($php_data->sitecode->id as $text)
     {
-    $speech = "Noooooo This question is too personal";
-    }    
-//    switch ($text) {
-//        case 'Name':
-//            $speech = "This question is too personal";
-//            break;
-//        case '555':
-//            $speech = "555 this ";
-//            break;
-//        case 'bye':
-//            $speech = "yes This question is too personal";
-//            break;
-//        case 'hi5':
-//            $speech = "Hi5 yes This question is too personal";
-//            break;
-//        default:
-//            $speech = "Sorry, I didnt get that 888.";
-//            break;
+        $speech = $php_data['sitecode'][0]['id'];
+    }
+
+//    if($text == $php_data['sitecode'][0]['id'])
+//    {
+//    $speech = "e001 This question is too personal";
+    
 //    }
+//    else
+//    {
+//    $speech = "Noooooo This question is too personal";
+//    }    
+
 
     $response = new \stdClass();
     $response->speech = $speech;
