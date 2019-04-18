41d26d637bbb022c1f3b1f0412dbd76e1c86d2d1
suttipow <49715933+suttipow@users.noreply.github.com>
2019-04-18 10:46:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if($method == 'POST'){
     //echo $json['sitecode'][1]['id'];
      $text = $json->result->parameters->text;
     
-    foeeach($php_data['sitecode']['id'] as $text)
+    foeeach($php_data['sitecode'] as $text)
     {
       
         $speech = $php_data['sitecode'][0]['id'];
