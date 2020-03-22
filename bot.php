8617f92084e74b886ad6b6f821700d838f1b930f
suttipow <49715933+suttipow@users.noreply.github.com>
2020-03-22 16:12:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -83,7 +83,7 @@ $php_data = json_decode($json_data,true);
         $arrayPostData['messages'][1]['previewImageUrl'] = $image_url;  
         
         $arrayPostData['messages'][2]['type'] = "text";
-        $arrayPostData['messages'][2]['text'] = "หากมีคำถาม\nให้พิมพ์คำว่า = ? ";
+        $arrayPostData['messages'][2]['text'] = "อยากทราบคำถาม\nที่พบบ่อยให้พิมพ์คำว่า = ? ";
 
         replyMsg($arrayHeader,$arrayPostData);
     }
